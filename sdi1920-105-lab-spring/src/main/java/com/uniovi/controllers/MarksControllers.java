7088886a517acf57b9c66a0dc5e5c430e23a2a5e
Andrew <Andresfdez9@gmail.com>
2020-02-21 22:46:07 +0100

    SDI-SpringBoot-Sesión-4.1-Sesiones

--- sdi1920-105-lab-spring/src/main/java/com/uniovi/controllers/MarksControllers.java
+++ sdi1920-105-lab-spring/src/main/java/com/uniovi/controllers/MarksControllers.java
@@ -1,5 +1,10 @@
 package com.uniovi.controllers;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +22,9 @@ import com.uniovi.services.UsersService;
 @Controller
 public class MarksControllers {
 	
+	@Autowired
+	private HttpSession httpSession;
+	
 	@Autowired //Inyectar el servicio
 	private MarksService marksService;
 
@@ -25,6 +33,8 @@ public class MarksControllers {
 
 	@RequestMapping("/mark/list" )
 	public String getList(Model model){
+		
+		
 		model.addAttribute("markList", marksService.getMarks() );
 		return "mark/list";
 		}
--- sdi1920-105-lab-spring/src/main/java/com/uniovi/services/MarksService.java
+++ sdi1920-105-lab-spring/src/main/java/com/uniovi/services/MarksService.java
@@ -1,10 +1,13 @@
 package com.uniovi.services;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,9 @@ import com.uniovi.repositories.MarksRepository;
 public class MarksService {
 
 	@Autowired
+	private HttpSession httpSession;
+	
+	@Autowired
 	private MarksRepository marksRepository;
 
 	public List<Mark> getMarks(){
@@ -25,8 +31,16 @@ public class MarksService {
 		return marks;
 	}
 	public Mark getMark(Long id){
-		return marksRepository.findById(id).get();
+		Set<Mark> consultedList = (Set<Mark>) httpSession.getAttribute("consultedList");
+		if ( consultedList == null ) {
+			consultedList = new HashSet<Mark>();
+		}
+		Mark obtainedmark = marksRepository.findById(id).get();
+		consultedList.add(obtainedmark);
+		httpSession.setAttribute("consultedList", consultedList);
+		return obtainedmark;
 	}
+	
 	public void addMark(Mark mark){
 		// Si en Id es null le asignamos el ultimo + 1 de la lista
 		marksRepository.save(mark);
@@ -36,4 +50,5 @@ public class MarksService {
 	}
 
 
+	
 }
